5042819e4f3810a8f6e45859d485b8335ab0620e
Felix Berman <felixberman76@gmail.com>
2019-09-25 09:51:46 +0300

    WIP - adapter

--- devtools/repluggableAdapter.js
+++ devtools/repluggableAdapter.js
@@ -42,17 +42,34 @@
   function createCodePathHostLogger(tracer) {
     return {
       event(severity, id, keyValuePairs, spanFlag) { 
+        const serializableKeyValuePairs = makeSerializable(keyValuePairs);
         switch (spanFlag) {
           case 'begin':
-            tracer.spanChild(id, keyValuePairs);
+            tracer.spanChild(id, serializableKeyValuePairs);
             break;
           case 'end':
             tracer.finishSpan();
             break;
           default:
-            tracer.logDebug(id, keyValuePairs);
+            tracer.logDebug(id, serializableKeyValuePairs);
         }
       }
+    };
+  }
+
+  function makeSerializable(data) {
+    if (typeof data.$api !== 'string' || !Array.isArray(data.$args)) {
+      return data;
+    }
+
+    return {
+      ...data,
+      $args: data.$args.map(arg => {
+        if (typeof arg === 'object' || typeof arg === 'function') {
+          return '...';
+        }
+        return arg;
+      })
     };
   }
 
